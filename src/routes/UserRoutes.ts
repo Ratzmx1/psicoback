ffdad946aac6cef46fa5988206c58b465b5b7662
LVarelaG <luxo_v7@hotmail.com>
2021-10-14 20:55:38 -0300

    Creado login y registro

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -2,45 +2,15 @@ import { Router } from "express";
 import { UsuarioI } from "../Interfaces/UsuarioI";
 import { UsuarioService } from "../services/UsurioServices";
 import jwt from "jsonwebtoken";
+import { login } from "../controllers/usuarios/loginController";
+import { register } from "../controllers/usuarios/registerController";
 
 const userRouter = Router();
 
 const userService = new UsuarioService();
 
-userRouter.post("/", async (req, res) => {
-  const { rut, nombre, correo, pass, nacimiento, sexo, telefono } = req.body;
+userRouter.post("/", register(userService));
 
-  const obtenidoRut: UsuarioI | null = await userService.obtenerPorrut(rut);
-  const obtenidoEmail: UsuarioI | null = await userService.obtenerPorEmail(
-    correo
-  );
-
-  if (obtenidoRut) {
-    return res.status(400).json({ message: "Usuario ya esta registrado" });
-  }
-
-  if (obtenidoEmail) {
-    return res
-      .status(400)
-      .json({ message: "El correo electronico ya esta en uso" });
-  }
-
-  const nacimientoDate = new Date(nacimiento);
-  const usuarioRegistrado = await userService.registro(
-    rut,
-    nombre,
-    correo,
-    pass,
-    nacimientoDate,
-    sexo,
-    telefono
-  );
-
-  const token = jwt.sign({ _id: usuarioRegistrado?._id }, "EstaEsLaKeyxd", {
-    expiresIn: "5 years",
-  });
-
-  return res.json({ user: usuarioRegistrado, token });
-});
+userRouter.post("/login", login(userService));
 
 export default userRouter;
